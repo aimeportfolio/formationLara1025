3467f978c19bc29e54fef9464dd279c8b14fe60b
Aimé TOSSOU - aimeportfolio <aisareltossou+portfolio@gmail.com>
2025-10-16 06:43:45 +0100

    Premier partie de la validation des données - test directemeent dans le controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,12 +7,24 @@ use App\Models\Post;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Validator;
 
 class BlogController extends Controller
 {
     public function index() {
+        //test de validation directement dans les controllers
+        $validator = Validator::make([
+            'title' => '123',
+            'content' => 'Bonjo'
+        ],[
+            'title' => 'required|string|min:6|max:10',
+            'content' => 'required|string|min:2|max:10',
+        ]);
+        return $validator->validated();
+
         $posts = Post::paginate(2);
         return view('blog.index',['posts' => $posts]);
+
     }
 
     
